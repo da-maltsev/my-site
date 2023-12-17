a7e16a692e275a26c5ca4682ed045ac579bbc0de
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-12-17 17:32:59 +0500

    add post detail page

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
@@ -9,14 +11,22 @@ from core.paths import setup_path
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=setup_path("static")), name="static")
 
-posts = [{"title": f"Title {i}", "content": "asdasdasdasdasdasdad" * 100} for i in range(24)]  # just developing mock
+posts = [
+    {"id": i, "title": f"Title {i}", "content": "asdasdasdasdasdasdad" * 100, "created": datetime.datetime.now(tz=datetime.timezone.utc)} for i in range(1, 6)
+]  # just developing mock
 
 
 @app.get("/", response_class=HTMLResponse)
 async def home_page(request: Request):
-    return templates.TemplateResponse("home/index.html", {"request": request, "posts": posts})
+    return templates.TemplateResponse("index.html", {"request": request, "posts": posts})
 
 
 @app.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
     return templates.TemplateResponse("about.html", {"request": request})
+
+
+@app.get("/posts/{post_id}", response_class=HTMLResponse)
+async def post_detail(request: Request, post_id: int):
+    post = next(post for post in posts if post["id"] == post_id)
+    return templates.TemplateResponse("post_detail.html", {"request": request, "post": post})
--- tests/pages/test_post.py
+++ tests/pages/test_post.py
@@ -1,6 +1,6 @@
 def test_ok(client):
-    got = client.get("/")
+    got = client.get("/posts/1")
 
     assert got.status_code == 200
-    assert got.template.name == "index.html"
+    assert got.template.name == "post_detail.html"
     assert "text/html" in got.headers["Content-Type"]
